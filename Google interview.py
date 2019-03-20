556afb16cea2906ce514607265a73a20ef5f3456
luka-bediashvili <44407979+luka-bediashvili@users.noreply.github.com>
2019-03-20 17:30:50 +0000

    Add files via upload

--- Google interview.py
+++ Google interview.py
@@ -33,8 +33,8 @@ class Main_body(tk.Frame):
             Lable1 = tk.Label(self.frame, text='Search for: ')
             Lable1.grid(row=1, column=1, sticky=W)
 
-            self.user_input = Label(self.frame, textvariable = self.text_show)
-            self.user_input.grid(row=1, column=2, sticky=W)
+            #self.user_input = Label(self.frame, textvariable = self.text_show)
+            #self.user_input.grid(row=1, column=2, sticky=W)
 
             button1 = ttk.Button(self.frame, text="Enter", command=lambda: self.show_data())
             button1.grid(row=1, column=7, sticky=W)
@@ -72,6 +72,8 @@ class Main_body(tk.Frame):
         window_1.title("Email")
         window_1.mainloop(self)
     def keydown(self,A):
+        self.user_input = Label(self.frame, textvariable = self.text_show)
+        self.user_input.grid(row=1, column=2, sticky=W)
         if A.char == "":
             self.text = self.text[:len(self.text)-1]
         else:
@@ -106,52 +108,52 @@ class Main_body(tk.Frame):
             data.execute(
                 'select Address from "Animal Data" where Address LIKE ?',(search,))
             self.Address=data.fetchall()
-##        try:
-##            if len(self.ID)!=0:
-##                button1 = ttk.Button(self.frame, text='Suggestion '+self.ID[0][0],width=30, command=lambda: self.update_text(self.ID[0][0]))
-##                button1.grid(row=2, column=1, sticky=W)
-##            else:
-##                Label_1 = Label(self.frame, text=' ',width=30)
-##                Label_1.grid(row=2, column=1, sticky=W)
-##            if len(self.Animal_Name)!=0:
-##                button2 = ttk.Button(self.frame, text='Suggestion '+self.Animal_Name[0][0],width=30,command=lambda: self.update_text(self.Animal_Name[0][0]))
-##                button2.grid(row=2, column=2, sticky=W)
-##            else:
-##                Label_2 = Label(self.frame, text=' ',width=30)
-##                Label_2.grid(row=2, column=2, sticky=W)
-##            if len(self.Animal_Type)!=0:
-##                button3 = ttk.Button(self.frame, text='Suggestion '+self.Animal_Type[0][0],width=30, command=lambda: self.update_text(self.Animal_Type[0][0]))
-##                button3.grid(row=2, column=3, sticky=W)
-##            else:
-##                Label_3 = Label(self.frame, text=' ',width=30)
-##                Label_3.grid(row=2, column=3, sticky=W)
-##            if len(self.Animal_Gender)!=0:
-##                button4 = ttk.Button(self.frame, text='Suggestion '+self.Animal_Gender[0][0],width=30, command=lambda: self.update_text(self.Animal_Gender[0][0]))
-##                button4.grid(row=2, column=4, sticky=W)
-##            else:
-##                Label_4 = Label(self.frame, text=' ',width=30)
-##                Label_4.grid(row=2, column=4, sticky=W)
-##            if len(self.Animal_breed)!=0:
-##                button5 = ttk.Button(self.frame, text='Suggestion '+self.Animal_breed[0][0],width=30, command=lambda: self.update_text(self.Animal_breed[0][0]))
-##                button5.grid(row=2, column=5, sticky=W)
-##            else:
-##                Label_5 = Label(self.frame, text=' ',width=30)
-##                Label_5.grid(row=2, column=5, sticky=W)
-##            if len(self.Animal_colour)!=0:
-##                button6 = ttk.Button(self.frame, text='Suggestion '+self.Animal_colour[0][0],width=30, command=lambda: self.update_text(self.Animal_colour[0][0]))
-##                button6.grid(row=2, column=6, sticky=W)
-##            else:
-##                Label_6 = Label(self.frame, text=' ',width=30)
-##                Label_6.grid(row=2, column=6, sticky=W)
-##            if len(self.Address)!=0:
-##                button7 = ttk.Button(self.frame, text='Suggestion '+self.Address[0][0],width=50, command=lambda: self.update_text(self.Address[0][0]))
-##                button7.grid(row=2, column=7, sticky=W)
-##            else:
-##                Label_7 = Label(self.frame, text=' ',width=50)
-##                Label_7.grid(row=2, column=7, sticky=W)
-##        except IndexError:
-##            tk.messagebox.showinfo("ERROR", "No valid search")
-##            window.destroy() 
+        try:
+            if len(self.ID)!=0:
+                button1 = ttk.Button(self.frame, text='Suggestion '+self.ID[0][0],width=30, command=lambda: self.update_text(self.ID[0][0]))
+                button1.grid(row=2, column=1, sticky=W)
+            else:
+                Label_1 = Label(self.frame, text=' ',width=30)
+                Label_1.grid(row=2, column=1, sticky=W)
+            if len(self.Animal_Name)!=0:
+                button2 = ttk.Button(self.frame, text='Suggestion '+self.Animal_Name[0][0],width=30,command=lambda: self.update_text(self.Animal_Name[0][0]))
+                button2.grid(row=2, column=2, sticky=W)
+            else:
+                Label_2 = Label(self.frame, text=' ',width=30)
+                Label_2.grid(row=2, column=2, sticky=W)
+            if len(self.Animal_Type)!=0:
+                button3 = ttk.Button(self.frame, text='Suggestion '+self.Animal_Type[0][0],width=30, command=lambda: self.update_text(self.Animal_Type[0][0]))
+                button3.grid(row=2, column=3, sticky=W)
+            else:
+                Label_3 = Label(self.frame, text=' ',width=30)
+                Label_3.grid(row=2, column=3, sticky=W)
+            if len(self.Animal_Gender)!=0:
+                button4 = ttk.Button(self.frame, text='Suggestion '+self.Animal_Gender[0][0],width=30, command=lambda: self.update_text(self.Animal_Gender[0][0]))
+                button4.grid(row=2, column=4, sticky=W)
+            else:
+                Label_4 = Label(self.frame, text=' ',width=30)
+                Label_4.grid(row=2, column=4, sticky=W)
+            if len(self.Animal_breed)!=0:
+                button5 = ttk.Button(self.frame, text='Suggestion '+self.Animal_breed[0][0],width=30, command=lambda: self.update_text(self.Animal_breed[0][0]))
+                button5.grid(row=2, column=5, sticky=W)
+            else:
+                Label_5 = Label(self.frame, text=' ',width=30)
+                Label_5.grid(row=2, column=5, sticky=W)
+            if len(self.Animal_colour)!=0:
+                button6 = ttk.Button(self.frame, text='Suggestion '+self.Animal_colour[0][0],width=30, command=lambda: self.update_text(self.Animal_colour[0][0]))
+                button6.grid(row=2, column=6, sticky=W)
+            else:
+                Label_6 = Label(self.frame, text=' ',width=30)
+                Label_6.grid(row=2, column=6, sticky=W)
+            if len(self.Address)!=0:
+                button7 = ttk.Button(self.frame, text='Suggestion '+self.Address[0][0],width=35, command=lambda: self.update_text(self.Address[0][0]))
+                button7.grid(row=2, column=7, sticky=W)
+            else:
+                Label_7 = Label(self.frame, text=' ',width=35)
+                Label_7.grid(row=2, column=7, sticky=W)
+        except IndexError:
+            tk.messagebox.showinfo("ERROR", "No valid search")
+            window.destroy() 
 
 
 
@@ -178,7 +180,7 @@ class Main_body(tk.Frame):
         button3 = ttk.Button(self.frame, text='previous 5', command=lambda: self.subtract())
         button3.grid(row=3, column=1, sticky=W)
         button3 = ttk.Button(self.frame, text='Reload', command=lambda: self.reload())
-        button3.grid(row=3, column=3, sticky=W)       
+        button3.grid(row=1, column=8, sticky=W)       
         if len(self.data)>0:
             y=5
             for i in range(self.count-5,self.count):
@@ -190,19 +192,19 @@ class Main_body(tk.Frame):
                     result_img.image = photo
                     print(photo)
                     result_img.grid(row=y, column=8, sticky=W)
-                    result0 = Label(self.frame, text=self.data[i][0],width=25,background='White')
+                    result0 = Label(self.frame, text=self.data[i][0],width=15,background='White')
                     result0.grid(row=y, column=1, sticky=W)
-                    result1 = Label(self.frame, text=self.data[i][1],width=25,background='White')
+                    result1 = Label(self.frame, text=self.data[i][1],width=20,background='White')
                     result1.grid(row=y, column=2, sticky=W)
-                    result2 = Label(self.frame, text=self.data[i][2],width=25,background='White')
+                    result2 = Label(self.frame, text=self.data[i][2],width=15,background='White')
                     result2.grid(row=y, column=3, sticky=W)
-                    result3 = Label(self.frame, text=self.data[i][3],width=25,background='White')
+                    result3 = Label(self.frame, text=self.data[i][3],width=15,background='White')
                     result3.grid(row=y, column=4, sticky=W)
                     result4 = Label(self.frame, text=self.data[i][4],width=25,background='White')
                     result4.grid(row=y, column=5, sticky=W)
                     result5 = Label(self.frame, text=self.data[i][5],width=25,background='White')
                     result5.grid(row=y, column=6, sticky=W)
-                    result6 = Label(self.frame, text=self.data[i][6],width=50,background='White')
+                    result6 = Label(self.frame, text=self.data[i][6],width=35,background='White')
                     result6.grid(row=y, column=7, sticky=W)
                     y+=1
         else:
@@ -215,6 +217,7 @@ class Main_body(tk.Frame):
         self.bind("<KeyPress>",self.keydown)
         self.grid()
         self.focus_set()
+        self.text_show.set('TEST.TWO')
 
 class Email(tk.Frame):
     def __init__(self, parent):
